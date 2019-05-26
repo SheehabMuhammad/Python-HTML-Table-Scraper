7ecbcd3cb3187a2369e323fc6487b98266e96150
Muhammad Sheehab <sheehabx@gmail.com>
2019-05-27 04:10:51 +0600

    Update exporter.py

--- pyscraper/exporter.py
+++ pyscraper/exporter.py
@@ -12,13 +12,13 @@ class Exporter(data):
 	def exportJSON(self, filename = 'data.json'):
 		with open(filename, 'w') as outfile:  
     		json.dump(self.data, outfile)
-    	print("Succesfully Exported to ", name)
+    		print("Succesfully Exported to ", name)
 
-    def exportCSV(self, filename = 'data.csv'):
+    	def exportCSV(self, filename = 'data.csv'):
 		with open(filename, mode='w') as csv_file:
      		csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
      		for item in self.data:
      			csv_writer.writerow(item)
-    	print("Succesfully Exported to ", name)
+    		print("Succesfully Exported to ", name)
 
 
